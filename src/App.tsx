8a8a393a213523928d5731b68e4dd43832b79472
Natalia Novak <nnovak@hiya.com>
2023-03-11 17:02:46 +0100

    serve svgs

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import flag from "./images/hu.svg";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <img src={flag} alt="" />
+    </div>
+  );
 }
 
 export default App;
