36a5ef1304b63fc35cc90a0e15c7107465c4add1
Ildikó Szabó <szab.ildiko89@gmail.com>
2023-05-30 18:14:00 +0200

    dropdownmenu fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { css, Skeleton, Button } from '@mui/material';
+import { css, Button } from '@mui/material';
 import FlagComponent from './FlagComponent';
 import DropdownMenu from './DropdownMenu.';
 
@@ -12,49 +12,39 @@ const headerCss = css`
   padding: 10px 0;
 `;
 
-const FlagSelector = () => (
-  // majd ez fog allapottol fuggoen Autocomplete-et megjeleniteni
-  // ez a rész a 6 válasz lehetőség
-  <Skeleton
-    animation={false}
-    variant='rounded'
-    sx={{
-      height: '30px',
-      margin: '20px 0',
-    }}
-  />
-);
+
 
 function App() {
+  const handleMenuChange = (index: number, value: string) => {
+    // Itt kezelheted a kiválasztott országokat
+    console.log(`Selected option ${index + 1}: ${value}`);
+  };
+
   return (
     <div
       style={{
-        // ez a rész a zászló képe
         width: '500px',
         margin: 'auto',
         textAlign: 'center',
-      }}>
+      }}
+    >
       <div
         style={{
-          // ez a rész a Flag-tips szöveg
           textTransform: 'uppercase',
           fontSize: '40px',
           fontWeight: 'bold',
           borderBottom: '2px solid rgba(0, 0, 0, 0.11)',
           marginBottom: '20px',
           padding: '10px 0',
-        }}>
+        }}
+      >
         Flag - Tips
       </div>
       <FlagComponent />
-      <DropdownMenu  /> 
+      <DropdownMenu handleMenuChange={handleMenuChange} 
+      />
       <div>
-        {Array(6)
-          .fill(null)
-          .map((_, i) => (
-            <FlagSelector key={i} />
-          ))}
-        <Button // tipp leadás gombja 
+        <Button
           style={{
             color: 'black',
             fontSize: '20px',
@@ -62,7 +52,8 @@ function App() {
             border: '3px solid rgba(0, 0, 0, 0.2)',
             background: 'grey',
           }}
-          onClick={() =>{} }>
+          onClick={() => {}}
+        >
           Guess
         </Button>
       </div>
@@ -71,3 +62,4 @@ function App() {
 }
 
 export default App;
+  
--- src/DropdownMenu..tsx
+++ src/DropdownMenu..tsx
@@ -1,30 +1,63 @@
 import React, { useState } from 'react';
-import { Autocomplete, TextField } from '@mui/material';
-import countries from './countries.json';
+import { css, Autocomplete, TextField } from '@mui/material';
+import countriesData from './countries.json';
+
+const countries: { [key: string]: string } = countriesData; // mivel a json fájlban nincs index definiálva, ezért nem tudja használni a string kifejezést. ezért kell ez a változó.
 
 interface AutocompleteOption {
-    label: string;
-  }
+  label: string;
+}
 
-const DropdownMenu = () => { 
-    const [selectedOption, setSelectedOption] = useState(null);
-    
-    const handleMenuChange = (event, target) => { 
-        setSelectedOption(value);
-    };
+const DropdownMenu = ({
+  handleMenuChange,
+}: {
+  handleMenuChange: (index: number, value: string) => void;
+}) => {
+  // ez a legördülő menü
+  const [selectedOptions, setSelectedOptions] = useState<Array<string>>(
+    Array(6).fill('')
+  );
 
+  const handleChange = (
+    event: React.ChangeEvent<{}>,
+    value: AutocompleteOption | null,
+    index: number
+  ) => {
+    if (value) {
+      setSelectedOptions(prevSelectedOptions => {
+        const newSelectedOptions = [...prevSelectedOptions];
+        newSelectedOptions[index] = value.label;
+        return newSelectedOptions;
+      });
+      handleMenuChange(index, value.label);
+    }
+  };
 
-    return (
-        <div>
-            <Autocomplete
-                options={Object.keys(countries)}
-                getOptionLabel={(countryCode) => countries[countryCode]} // beállítjuk a megjelenített szöveget az országkódhoz tartozó országnév alapján
-                value={selectedOption}
-                onChange={handleMenuChange} // eseménykezelő, ami frissíti a kiválasztott opciót az állapotban (selectedOption)
-                renderInput={(params) => <TextField {...params} label="What is your tip?"/>}
-            />
-        </div>
-    );
+  return (
+    <div>
+      {selectedOptions.map((selectedOption, index) => (
+        <Autocomplete
+          key={index}
+          options={Object.keys(countries)}
+          getOptionLabel={countryCode => countries[countryCode]} // beállítjuk a megjelenített szöveget az országkódhoz tartozó országnév alapján
+          value={selectedOption}
+          onChange={(event, value) =>
+            handleChange(event, value ? { label: value } : null, index)
+          }
+          renderInput={params => (
+            <TextField {...params} />
+          )}
+          style={{
+            color: 'black',
+            fontWeight: 'bold',
+            border: '3px solid rgba(0, 0, 0, 0.2)',
+            marginBottom: '5px',
+            marginTop: '5px'
+          }}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default DropdownMenu;
